3d765c828553327a2604df9342fcda6efc10482a
Yoni Gray <ramenkage@gmail.com>
2022-05-03 15:21:06 -0400

    Removed deprecated use of Ember.get() in serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,10 +1,9 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
-import { get } from '@ember/object';
 
 export default class ApplicationSerializer extends JSONAPISerializer {
   normalize(typeClass, hash) {
     // Move attributes beneath the 'attributes' key as expected by JSON:API
-    const fields = get(typeClass, 'fields');
+    const fields = typeClass.fields;
     hash.attributes = {};
 
     fields.forEach(function (type, field) {
