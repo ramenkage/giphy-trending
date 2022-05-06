a3cc131d8729350a86d1158b2f321e96e84069a7
Yoni Gray <ramenkage@gmail.com>
2022-05-06 13:24:44 -0400

    Added fancy toggle styling for radio buttons

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,14 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class IndexController extends Controller {
   @service media;
   @tracked animate = !this.media.isReducedMotion;
+
+  @action
+  toggleAnimate() {
+    this.animate = !this.animate;
+  }
 }
